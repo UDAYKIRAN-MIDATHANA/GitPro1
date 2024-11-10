608846225f6845c9ccce85f2c7d4d60a9d062035
uday kiran <uday kiran@LAPTOP-LT9GQKNO>
2024-11-10 10:56:51 +0530

    dev1 commir

--- src/sample01git/HelloWorld.java
+++ src/sample01git/HelloWorld.java
@@ -2,7 +2,7 @@ package sample01git;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello kiran");
 		System.out.println("thank you");
 	}
 
